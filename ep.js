2464ecd6ef0656540abb869198bd0505a31d8b52
ibrar5 <83902973+ibrar5@users.noreply.github.com>
2021-12-13 20:32:19 +0600

    Add files via upload

---
+++ ep.js
@@ -0,0 +1,327 @@
+function dub(){
+    if(document.getElementById("inp").checked){
+        document.getElementById("dub").style.display ="block";
+        document.getElementById("sub").style.display="none";
+        document.getElementById("dub").style.padding="0px";
+    }else {
+        document.getElementById("dub").style.display ="none";
+        document.getElementById("sub").style.display="block";
+        document.getElementById("sub").style.padding="0px";
+    }
+}
+
+function anime(){
+    var item_1 = document.getElementById("search").value;
+    item = item_1.toLowerCase();
+    if(item === "attack on titan season 3 part 2"){
+        window.location = "epaot3p2.html";
+    }
+    if(item === "attack on titan"){
+        window.location = "epaot1.html";
+    }
+    if(item === "attack on titan season 1"){
+        window.location = "epaot1.html";
+    }
+    if(item === "attack on titan season 2"){
+        window.location = "epaot2.html";
+    }
+    if(item === "attack on titan season 3"){
+        window.location = "epaot3.html";
+    }
+    if(item === "attack on titan the final season"){
+        window.location = "epaot4.html";
+    }
+    if(item === "attack on titan season 4"){
+        window.location = "epaot4.html";
+    }
+    if(item === "code geass"){
+        window.location = "epcg.html";
+    }
+    if(item === "code geass r2"){
+        window.location = "epcg2.html";
+    }
+    if(item === "code geass lelouch of the rebellion"){
+        window.location = "epcg.html";
+    }
+    if(item === "code geass lelouch of the rebellion r2"){
+        window.location = "epcg2.html";
+    }
+    if(item === "code geass lelouch of the resurrection"){
+        window.location = "epcgre.html";
+    }
+    if(item === "code geass movies"){
+        window.location = "epcgm.html";
+    }
+    if(item === "code geass akito the exciled"){
+        window.location = "epcga.html";
+    }
+    if(item === "code geass season 2"){
+        window.location = "epcg2.html";
+    }
+    if(item === "death note"){
+        window.location = "epdn.html";
+    }
+    if(item === "demon slayer"){
+        window.location = "epds.html";
+    }
+    if(item === "demon slayer mugen train movie"){
+        window.location = "epdsm.html";
+    }
+    if(item === "demon slayer infinite train arc movie"){
+        window.location = "epdsm.html";
+    }
+    if(item === "demon slayer mugen train"){
+        window.location = "epdsmt.html";
+    }
+    if(item === "demon slayer infinite train arc"){
+        window.location = "epdsmt.html";
+    }
+    if(item === "demon slayer: mugen train"){
+        window.location = "epdsm.html";
+    }
+    if(item === "demon slayer: infinnite train"){
+        window.location = "epdsm.html";
+    }
+    if(item === "kimetsu no yaiba"){
+        window.location = "epds.html";
+    }
+    if(item === "demon slayer entertainment district arc"){
+        window.location = "epds2.html";
+    }
+    if(item === "demon slayer season 2"){
+        window.location = "epds2.html";
+    }
+    if(item === "fullmetal alchemist: brotherhood"){
+        window.location = "epfmab.html";
+    }
+    if(item === "fullmetal alchemist brotherhood"){
+        window.location = "epfmab.html";
+    }
+    if(item === "hunter x hunter"){
+        window.location = "ephxh.html";
+    }
+    if(item === "jujutsu kaisen"){
+        window.location = "epjk.html";
+    }
+    if(item === "my hero academia"){
+        window.location = "epmha1.html";
+    }
+    if(item === "my hero academia season 1"){
+        window.location = "epmha1.html";
+    }
+    if(item === "my hero academia season 2"){
+        window.location = "epmha2.html";
+    }
+    if(item === "my hero academia season 3"){
+        window.location = "epmha3.html";
+    }
+    if(item === "my hero academia season 4"){
+        window.location = "epmha4.html";
+    }
+    if(item === "my hero academia season 5"){
+        window.location = "epmha5.html";
+    }
+    if(item === "naruto"){
+        window.location = "epnaruto.html";
+    }
+    if(item === "naruto the last"){
+        window.location = "epnlast.html";
+    }
+    if(item === "naruto: the last"){
+        window.location = "epnlast.html";
+    }
+    if(item === "naruto: the last movie"){
+        window.location = "epnlast.html";
+    }
+    if(item === "naruto shippuden"){
+        window.location = "epns.html";
+    }
+    if(item === "boruto"){
+        window.location = "epboruto.html";
+    }
+    if(item === "boruto naruto next generation"){
+        window.location = "epboruto.html";
+    }
+    if(item === "boruto: naruto next generation"){
+        window.location = "epboruto.html";
+    }
+    if(item === "one punch man"){
+        window.location = "epopm.html";
+    }
+    if(item === "one punch man season 2"){
+        window.location = "epopm2.html";
+    }
+    if(item === "tokyo ghoul"){
+        window.location = "eptg.html";
+    }
+    if(item === "tokyo ghoul season 2"){
+        window.location = "eptg2.html";
+    }
+    if(item === "tokyo ghoul re"){
+        window.location = "eptgre.html";
+    }
+    if(item === "tokyo ghoul: re"){
+        window.location = "eptgre.html";
+    }
+    if(item === "tokyo ghoul re season 2"){
+        window.location = "eptgre2.html";
+    }
+    if(item === "tokyo ghoul: re season 2"){
+        window.location = "eptgre2.html";
+    }
+    if(item === "vinland saga"){
+        window.location = "epvs.html";
+    }
+    if(item === "tokyo revengers"){
+        window.location = "eptr.html";
+    }
+    if(item === "your name"){
+        window.location = "epyn.html";
+    }
+    if(item === "haikyuu"){
+        window.location = "ephky.html";
+    }
+    if(item === "haikyuu!!"){
+        window.location = "ephky.html";
+    }
+    if(item === "haikyu"){
+        window.location = "ephky.html";
+    }
+    if(item === "haikyu!!"){
+        window.location = "ephky.html";
+    }
+    if(item === "haikyuu season 2"){
+        window.location = "ephky2.html";
+    }
+    if(item === "haikyuu!! season 2"){
+        window.location = "ephky2.html";
+    }
+    if(item === "haikyuu!! ova riku vs kuu"){
+        window.location = "ephkyova.html";
+    }
+    if(item === "haikyu season 2"){
+        window.location = "ephky2.html";
+    }
+    if(item === "haikyu!! season 2"){
+        window.location = "ephky2.html";
+    }
+    if(item === "haikyu season 3"){
+        window.location = "ephky3.html";
+    }
+    if(item === "haikyu!! season 3"){
+        window.location = "ephky3.html";
+    }
+    if(item === "haikyuu!! karasuno high school vs shiratorizawa academy"){
+        window.location = "ephky3.html";
+    }
+    if(item === "haikyuu karasuno high school vs shiratorizawa academy"){
+        window.location = "ephky3.html";
+    }
+    if(item === "karasuno high school vs shiratorizawa academy"){
+        window.location = "ephky3.html";
+    }
+    if(item === "haikyu season 4"){
+        window.location = "ephky4.html";
+    }
+    if(item === "haikyu!! season 4"){
+        window.location = "ephky4.html";
+    }
+    if(item === "haikyuu!! to the top"){
+        window.location = "ephky4.html";
+    }
+    if(item === "haikyuu to the top season 2"){
+        window.location = "ephky4p2.html";
+    }
+    if(item === "haikyuu!! to the top season 2"){
+        window.location = "ephky4p2.html";
+    }
+    if(item === "haikyuu to the top"){
+        window.location = "ephky4.html";
+    }
+    if(item === "hanako kun"){
+        window.location = "eptbh.html";
+    }
+    if(item === "hanako-kun"){
+        window.location = "eptbh.html";
+    }
+    if(item === "toilet bound hanako-kun"){
+        window.location = "eptbh.html";
+    }
+    if(item === "toilet bound hanako kun"){
+        window.location = "eptbh.html";
+    }
+    if(item === "dr stone"){
+        window.location = "epdr.html";
+    }
+    if(item === "doctor stone"){
+        window.location = "epdr.html";
+    }
+    if(item === "dr stone season 2"){
+        window.location = "epdr2.html";
+    }
+    if(item === "doctor stone season 2"){
+        window.location = "epdr2.html";
+    }
+    if(item === "dr stone: stone wars"){
+        window.location = "epdr2.html";
+    }
+    if(item === "doctor stone: stone wars"){
+        window.location = "epdr2.html";
+    }
+    if(item === "dr stone stone wars"){
+        window.location = "epdr2.html";
+    }
+    if(item === "doctor stone stone wars"){
+        window.location = "epdr2.html";
+    }
+    if(item === "your lie in april"){
+        window.location = "epyl.html";
+    }
+    if(item === "mob psycho"){
+        window.location = "epmob.html";
+    }
+    if(item === "mob psycho 100"){
+        window.location = "epmob.html";
+    }
+    if(item === "mob psycho season 2"){
+        window.location = "epmob2.html";
+    }
+    if(item === "mob psycho 100 season 2"){
+        window.location = "epmob2.html";
+    }
+    if(item === "mob psycho 100 ii"){
+        window.location = "epmob2.html";
+    }
+    if(item === "cowboy bebop"){
+        window.location = "epcb.html";
+    }
+    if(item === "legend of the galactic heroes"){
+        window.location = "epleg.html";
+    }
+}
+function change(){
+    document.getElementById("sign").style.display ="block";
+    document.getElementById("log").style.display="none";
+}
+function change2(){
+    document.getElementById("sign").style.display ="none";
+    document.getElementById("log").style.display="block";
+}
+function rate(){
+    document.getElementById("hid").style.display = "none";
+    document.getElementById("rate").style.display = "block";
+}
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function mic(){
+    recognition.start();
+}
+recognition.onresult = function(event){
+    console.log(event)
+    var content = event.results[0][0].transcript;
+    console.log(content);
+
+    document.getElementById("fa").innerHTML = content;
+}
